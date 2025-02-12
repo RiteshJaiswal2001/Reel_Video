fb913660e33a55c91544a0715f453b1650cc6a7f
Ritesh Kumar <rkjais10022001@gmail.com>
2025-02-12 08:07:13 +0530

    first commit

---
+++ lib/db.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("Please definr mongodb.");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true, //homework
+      maxPoolSize: 10, // how many connection can mongodb have at a time.
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}
